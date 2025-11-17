e98b28930dbb7ce66465e886c57396b07241efd9
Ralph-55 <rserra.0109@unimar.edu.ve>
2025-11-17 16:07:53 -0400

    2123

--- FRONTEND/src/pages/auth/RegisterPrompt.jsx
+++ FRONTEND/src/pages/auth/RegisterPrompt.jsx
@@ -1,15 +1,16 @@
 // src/components/RegisterPrompt.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function RegisterPrompt() {
   return (
     <div className="flex-1 bg-blue-800 text-white flex flex-col items-center justify-center p-8 rounded-r-lg">
       <h2 className="text-4xl font-bold mb-6 text-center">¿AÚN NO ESTÁS REGISTRADO?</h2>
-      <a href="./Register">
+      <link href="./Register">
         <button className="bg-white text-blue-800 py-3 px-8 rounded-full hover:bg-gray-100 transition duration-200 font-semibold">
           Registrarse
         </button>
-      </a>
+      </link>
     </div>
   );
 }
